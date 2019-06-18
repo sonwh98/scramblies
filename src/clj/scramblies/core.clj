43ee7944e15a0ba8cb538c37dd34cd5e58fed11f
sto <son.c.to@gmail.com>
2019-06-18 15:33:20 +0800

    scramble? works

--- src/clj/scramblies/core.clj
+++ src/clj/scramblies/core.clj
@@ -2,16 +2,26 @@
   (:require [clojure.string :as str]
             [clojure.set :as set]))
 
-
 (defn normalize [a-str]
   (-> a-str
       str/lower-case
-      )
+      ))
+
+(defn subset? [c a-set]
+  (set/subset? #{c} (set a-set)))
+
+(defn scramble? [str1 str2]
+  (let [set-a (-> str1 normalize set)
+        set-b (-> str2 normalize set)
+        subset-a (set/select (fn [c]
+                               (subset? c set-b))
+                             set-a)]
+    (= subset-a set-b)))
+
+(comment
+  (scramble? "cedewaraaossoqqyt" "codewars")
+  (scramble? "rekqodlw" "world")
+
+  (scramble? "katas" "steak")
+  
   )
-
-(defn scramble [str1 str2]
-  (let [set1 (-> str1 normalize set)
-        set2 (-> str1 normalize set)]
-    (for [c set2]
-      (set/subset? #{c} set1))))
-

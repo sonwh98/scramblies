eb3c217db05840a2ac6b4b0923888b5e24362b0a
sto <son.c.to@gmail.com>
2019-06-19 15:16:13 +0800

    documentation

--- src/clj/scramblies/core.clj
+++ src/clj/scramblies/core.clj
@@ -16,16 +16,18 @@
       str/trim))
 
 (defn min-frequencies-met?
-  "returns true if frequencies of str1 is greater than or equal to frequencies of str2"
+  "calculates frequencies of letters in str1 and str2.
+  returns true of frequencies of letters in str1 is at least the same frequencies as in str2"
   [str1 str2]
   (let [freq-a (frequencies str1)
-        freq-b (frequencies str2)]
+        freq-b (frequencies str2)
+        comparisons (for [[ch count-b] freq-b
+                          :let [count-a (freq-a ch) ]]
+                      (if count-a
+                        (- count-a count-b)
+                        -1))]
     (every? #(>= % 0)
-            (for [[ch count-b] freq-b
-                  :let [count-a (freq-a ch) ]]
-              (if count-a
-                (- count-a count-b)
-                -1)))))
+            comparisons)))
 
 (defn scramble? [str1 str2]
   (let [str1 (normalize str1)

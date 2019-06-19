c1456a9065e909d62b8730f6182588f5010c8cd1
sto <son.c.to@gmail.com>
2019-06-19 14:36:41 +0800

    ajax call working

--- src/cljs/scramblies/core.cljs
+++ src/cljs/scramblies/core.cljs
@@ -1,9 +1,42 @@
-(ns scramblies.core)
+(ns scramblies.core
+  (:require [reagent.core :as r]
+            [reagent.dom :as dom]
+            [goog.Uri]
+            [goog.net.XhrIo :as xhr]
+            [cljs.reader]))
 
 (enable-console-print!)
+(def app-state (r/atom {}))
+
+(defn get-value [id]
+  (.. (js/document.getElementById id)
+      -value))
+
+(defn scramble-ui [state]
+  [:div
+   "(scramble "
+   [:input#str1 {:placeholder "str1"}]
+   [:input#str2 {:placeholder "str2"}]
+   ")  "
+   "= " (-> @state :result str)
+   [:br]
+   [:button {:on-click (fn [evt]
+                         (let [str1 (get-value "str1")
+                               str2 (get-value "str2")
+                               uri-str (str "/scramble?str1=" str1 "&str2=" str2)
+                               uri (goog.Uri. uri-str)]
+                           (prn "uri-str=" uri-str)
+                           (xhr/send uri  (fn [reply]
+                                            (let [edn-str (.. reply -target getResponse)
+                                                  edn (cljs.reader/read-string edn-str)
+                                                  result (:result edn)]
+                                              (prn "result=" result)
+                                              (swap! state assoc :result result)))))
+                         )} "evaluate"]]
+  )
 
 (defn init []
-  (prn "init")
+  (r/render-component [scramble-ui app-state] (js/document.getElementById "app"))    
   )
 
 (init)

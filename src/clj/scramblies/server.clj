da7373b01f8930463ecd0bfed66ed21154739a99
sto <son.c.to@gmail.com>
2019-06-19 12:48:41 +0800

    http call /scramble

--- src/clj/scramblies/server.clj
+++ src/clj/scramblies/server.clj
@@ -1,18 +1,23 @@
 (ns scramblies.server
   (:require [compojure.core :refer [defroutes GET]]
             [compojure.route :as r]
+            [ring.middleware.params :as ring]
             [org.httpkit.server :as h]
-            [taoensso.timbre :as log :include-macros true])
+            [taoensso.timbre :as log :include-macros true]
+            [scramblies.core :as s])
   (:gen-class))
 
 (defroutes app
+  (GET "/scramble" [str1 str2 :as req]
+       (str {:args [str1 str2]
+             :result (s/scramble? str1 str2)}))
   (r/resources "/")
   (r/not-found "<h1>Route not defined</h1>"))
 
 (defonce stop-fn (atom nil))
 
 (defn start []
-  (reset! stop-fn (h/run-server app {:port 3004})))
+  (reset! stop-fn (h/run-server (ring/wrap-params app) {:port 3000})))
 
 (defn stop []
   (when @stop-fn
